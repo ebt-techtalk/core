65e8285647033b5626e31e2b18b648267f573c78
ebt-techtalk <dpodrezov@eastbanctech.com>
2016-08-09 13:52:38 -0400

    demo.webapi

--- src/demo.webapi/Startup.cs
+++ src/demo.webapi/Startup.cs
@@ -29,6 +29,7 @@ namespace demo.webapi
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,10 +42,7 @@ namespace demo.webapi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("demo.webapi");
-            });
+            app.UseMvc();
         }
     }
 }
