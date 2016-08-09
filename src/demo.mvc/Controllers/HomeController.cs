f5bdbd34c9b4680e56731b1818231ef22c14e372
Dmitriy Podrezov <dpodrezov@eastbanctech.com>
2016-08-09 13:48:05 -0400

    demo.mvc

---
+++ src/demo.mvc/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo.ef.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "demo.mvc";
+            ViewBag.Message2 = "view bag message";
+            return View();
+        }
+    }
+}
