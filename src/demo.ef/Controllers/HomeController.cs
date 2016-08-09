c2c46a1c753ac91083b14a11880f3d0f97fe0bcc
Dmitriy Podrezov <dpodrezov@eastbanctech.com>
2016-08-09 14:05:30 -0400

    demo.ef

--- src/demo.ef/Controllers/HomeController.cs
+++ src/demo.ef/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using demo.ef.Models;
 
 namespace demo.ef.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -17,13 +25,20 @@ namespace demo.ef.Controllers
         {
             ViewData["Message"] = "Your application description page.";
 
-            return View();
+            return View(_context.Users.ToList());
         }
 
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
-
+            if (!_context.Users.Any())
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    _context.Users.Add(new User { FirstName = "User", LastName = "number " + i });
+                }
+                _context.SaveChanges();
+            }
             return View();
         }
 
---
+++ src/demo.ef/Migrations/20160809180333_Initial.Designer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using demo.ef.Models;
+
+namespace demo.ef.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    [Migration("20160809180333_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("demo.ef.Models.User", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("FirstName");
+
+                    b.Property<string>("LastName");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Users");
+                });
+        }
+    }
+}
