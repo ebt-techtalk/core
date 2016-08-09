45ad2936c2fa6c361ead2715655a2ea5b9047b80
Dmitriy Podrezov <dpodrezov@eastbanctech.com>
2016-08-09 13:41:00 -0400

    demo.configuration

---
+++ src/demo.configuration/CustomConfiguration.cs
@@ -0,0 +1,22 @@
+﻿namespace demo.configuration
+{
+    class CustomConfiguration
+    {
+        public ServiceClientConfiguration ServiceClient { get; set; }
+
+        public ServiceBusConfiguration ServiceBus { get; set; }
+
+        public class ServiceBusConfiguration
+        {
+            public string Endpoint { get; set; }
+            public string SharedAccessKeyName { get; set; }
+            public string SharedAccessKeyValue { get; set; }
+        }
+
+        public class ServiceClientConfiguration
+        {
+            public string ClientId { get; set; }
+            public string ClientSecret { get; set; }
+        }
+    }
+}
--- src/demo.configuration/Startup.cs
+++ src/demo.configuration/Startup.cs
@@ -5,17 +5,36 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace demo.configuration
 {
     public class Startup
     {
+        private IConfigurationRoot Configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets();
+            }
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions();
+            services.Configure<CustomConfiguration>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,7 +49,10 @@ namespace demo.configuration
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("demo.configuration");
+                var connectionString = Configuration["Data:DefaultConnection:ConnectionString"];
+                var configurationOptions = context.RequestServices.GetRequiredService<IOptions<CustomConfiguration>>();
+                var customConfiguration = configurationOptions.Value;
+                await context.Response.WriteAsync($"demo.configuration, connection string = {connectionString}, service bus endpoint = {customConfiguration.ServiceBus.Endpoint}");
             });
         }
     }
