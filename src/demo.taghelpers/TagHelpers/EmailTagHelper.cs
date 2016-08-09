a514a9417361caa6cef8c2583cc9c4391caa713d
Dmitriy Podrezov <dpodrezov@eastbanctech.com>
2016-08-09 14:26:27 -0400

    demo.taghelpers

---
+++ src/demo.taghelpers/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using demo.taghelpers.Configuration;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+namespace demo.taghelpers.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        private EmailConfiguration _configuration;
+
+        public EmailTagHelper(IOptions<EmailConfiguration> configuration)
+        {
+            _configuration = configuration.Value;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var address = content.GetContent() + "@" + _configuration.Domain;
+            output.Attributes.SetAttribute("href", "mailto:" + address);
+            output.Content.SetContent(address);
+        }
+    }
+}
