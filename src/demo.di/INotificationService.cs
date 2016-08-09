67e4657fe99589bd5db29d8ad0d426a494e5b75e
Dmitriy Podrezov <dpodrezov@eastbanctech.com>
2016-08-09 13:27:36 -0400

    demo.di

---
+++ src/demo.di/INotificationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace demo.di
+{
+    public interface INotificationService
+    {
+        Task NotifyUser(string email, string message);
+    }
+}
---
+++ src/demo.di/MailNotificationService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace demo.di
+{
+    public class MailNotificationService : INotificationService
+    {
+        public async Task NotifyUser(string email, string message)
+        {
+            // implementation
+        }
+    }
+}
