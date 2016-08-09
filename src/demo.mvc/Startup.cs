f538e62a5a771c28d785056cfc7bcb4107f49977
ebt-techtalk <dpodrezov@eastbanctech.com>
2016-08-09 13:48:41 -0400

    demo.mvc

--- src/demo.mvc/Startup.cs
+++ src/demo.mvc/Startup.cs
@@ -29,6 +29,7 @@ namespace demo.mvc
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,9 +42,11 @@ namespace demo.mvc
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("demo.mvc");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
